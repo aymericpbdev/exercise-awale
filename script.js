f5a8e7dbccc40966629f07080812d331cfd747d8
marwanahemm <marwan.ahemmane@gmail.com>
2025-11-14 10:02:18 +0100

    Add JS

---
+++ script.js
@@ -0,0 +1,17 @@
+function checkWin() {
+    // Calcule le total des graines sur le plateau
+    const totalSeedsOnBoard = gameState.board[0].reduce((a, b) => a+b, 0) +
+                              gameState.board[1].reduce((a, b) => a+b, 0);
+    }
+
+    // Vérifie si un joueur a capturé plus de 50% des graines (25 ou plus)
+    if (gameState.scores[0] >= 25){
+        messageEL.textContent = '🎯Joueur 1 atteint ${gameState.scores[0]} graines'
+        endgame();
+        return
+    }
+    if (gameState.scores[0] >= 25){
+        messageEL.textContent = '🎯Joueur 2 atteint ${gameState[0]} graines'
+    }
+    
+    // Vérifie si le plateau est vide (toutes les graines capturées)
